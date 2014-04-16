f1b9d538d0fec543c7a6605caa9db3d489c0e63b
Andelf <andelf@gmail.com>
2014-04-16 16:07:29 +0800

    primitives.rs: implement all fn

--- src/sdl2_gfx/lib.rs
+++ src/sdl2_gfx/lib.rs
@@ -13,18 +13,6 @@ A binding for SDL2_gfx.
 extern crate libc;
 extern crate sdl2;
 
-use libc::{c_int, c_long};
-use std::ptr;
-use std::fmt;
-use std::c_str::CString;
-use std::num::FromPrimitive;
-use sdl2::surface::Surface;
-use sdl2::get_error;
-use sdl2::pixels;
-use sdl2::pixels::Color;
-use sdl2::pixels::ll::SDL_Color;
-use sdl2::rwops::RWops;
-
 // Setup linking for all targets.
 #[link(name="SDL2_gfx")]
 extern {}
