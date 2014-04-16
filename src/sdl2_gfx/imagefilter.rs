56226ffcdf1b602c139d1bdcce8540d2dddc576b
Andelf <andelf@gmail.com>
2014-04-16 17:20:39 +0800

    add FPSManger, Rotozoom

--- src/sdl2_gfx/imagefilter.rs
+++ src/sdl2_gfx/imagefilter.rs
@@ -6,73 +6,73 @@ pub mod ll {
         pub fn SDL_imageFilterMMXdetect() -> c_int;
         pub fn SDL_imageFilterMMXoff();
         pub fn SDL_imageFilterMMXon();
-        pub fn SDL_imageFilterAdd(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                  Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterMean(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                   Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterSub(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                  Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterAbsDiff(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                      Dest: *mut c_uchar, length: c_uint) ->
+        pub fn SDL_imageFilterAdd(Src1: *u8, Src2: *u8,
+                                  Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterMean(Src1: *u8, Src2: *u8,
+                                   Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterSub(Src1: *u8, Src2: *u8,
+                                  Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterAbsDiff(Src1: *u8, Src2: *u8,
+                                      Dest: *u8, length: c_uint) ->
          c_int;
-        pub fn SDL_imageFilterMult(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                   Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterMultNor(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                      Dest: *mut c_uchar, length: c_uint) ->
+        pub fn SDL_imageFilterMult(Src1: *u8, Src2: *u8,
+                                   Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterMultNor(Src1: *u8, Src2: *u8,
+                                      Dest: *u8, length: c_uint) ->
          c_int;
-        pub fn SDL_imageFilterMultDivby2(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                         Dest: *mut c_uchar, length: c_uint) ->
+        pub fn SDL_imageFilterMultDivby2(Src1: *u8, Src2: *u8,
+                                         Dest: *u8, length: c_uint) ->
          c_int;
-        pub fn SDL_imageFilterMultDivby4(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                         Dest: *mut c_uchar, length: c_uint) ->
+        pub fn SDL_imageFilterMultDivby4(Src1: *u8, Src2: *u8,
+                                         Dest: *u8, length: c_uint) ->
          c_int;
-        pub fn SDL_imageFilterBitAnd(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                     Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterBitOr(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                    Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterDiv(Src1: *mut c_uchar, Src2: *mut c_uchar,
-                                  Dest: *mut c_uchar, length: c_uint) -> c_int;
-        pub fn SDL_imageFilterBitNegation(Src1: *mut c_uchar, Dest: *mut c_uchar,
+        pub fn SDL_imageFilterBitAnd(Src1: *u8, Src2: *u8,
+                                     Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterBitOr(Src1: *u8, Src2: *u8,
+                                    Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterDiv(Src1: *u8, Src2: *u8,
+                                  Dest: *u8, length: c_uint) -> c_int;
+        pub fn SDL_imageFilterBitNegation(Src1: *u8, Dest: *u8,
                                           length: c_uint) -> c_int;
-        pub fn SDL_imageFilterAddByte(Src1: *mut c_uchar, Dest: *mut c_uchar,
-                                      length: c_uint, C: c_uchar) -> c_int;
-        pub fn SDL_imageFilterAddUint(Src1: *mut c_uchar, Dest: *mut c_uchar,
+        pub fn SDL_imageFilterAddByte(Src1: *u8, Dest: *u8,
+                                      length: c_uint, C: u8) -> c_int;
+        pub fn SDL_imageFilterAddUint(Src1: *u8, Dest: *u8,
                                       length: c_uint, C: c_uint) -> c_int;
-        pub fn SDL_imageFilterAddByteToHalf(Src1: *mut c_uchar,
-                                            Dest: *mut c_uchar, length: c_uint,
-                                            C: c_uchar) -> c_int;
-        pub fn SDL_imageFilterSubByte(Src1: *mut c_uchar, Dest: *mut c_uchar,
-                                      length: c_uint, C: c_uchar) -> c_int;
-        pub fn SDL_imageFilterSubUint(Src1: *mut c_uchar, Dest: *mut c_uchar,
+        pub fn SDL_imageFilterAddByteToHalf(Src1: *u8,
+                                            Dest: *u8, length: c_uint,
+                                            C: u8) -> c_int;
+        pub fn SDL_imageFilterSubByte(Src1: *u8, Dest: *u8,
+                                      length: c_uint, C: u8) -> c_int;
+        pub fn SDL_imageFilterSubUint(Src1: *u8, Dest: *u8,
                                       length: c_uint, C: c_uint) -> c_int;
-        pub fn SDL_imageFilterShiftRight(Src1: *mut c_uchar, Dest: *mut c_uchar,
-                                         length: c_uint, N: c_uchar) -> c_int;
-        pub fn SDL_imageFilterShiftRightUint(Src1: *mut c_uchar,
-                                             Dest: *mut c_uchar, length: c_uint,
-                                             N: c_uchar) -> c_int;
-        pub fn SDL_imageFilterMultByByte(Src1: *mut c_uchar, Dest: *mut c_uchar,
-                                         length: c_uint, C: c_uchar) -> c_int;
-        pub fn SDL_imageFilterShiftRightAndMultByByte(Src1: *mut c_uchar,
-                                                      Dest: *mut c_uchar,
-                                                      length: c_uint, N: c_uchar,
-                                                      C: c_uchar) -> c_int;
-        pub fn SDL_imageFilterShiftLeftByte(Src1: *mut c_uchar,
-                                            Dest: *mut c_uchar, length: c_uint,
-                                            N: c_uchar) -> c_int;
-        pub fn SDL_imageFilterShiftLeftUint(Src1: *mut c_uchar,
-                                            Dest: *mut c_uchar, length: c_uint,
-                                            N: c_uchar) -> c_int;
-        pub fn SDL_imageFilterShiftLeft(Src1: *mut c_uchar, Dest: *mut c_uchar,
-                                        length: c_uint, N: c_uchar) -> c_int;
-        pub fn SDL_imageFilterBinarizeUsingThreshold(Src1: *mut c_uchar,
-                                                     Dest: *mut c_uchar,
-                                                     length: c_uint, T: c_uchar)
+        pub fn SDL_imageFilterShiftRight(Src1: *u8, Dest: *u8,
+                                         length: c_uint, N: u8) -> c_int;
+        pub fn SDL_imageFilterShiftRightUint(Src1: *u8,
+                                             Dest: *u8, length: c_uint,
+                                             N: u8) -> c_int;
+        pub fn SDL_imageFilterMultByByte(Src1: *u8, Dest: *u8,
+                                         length: c_uint, C: u8) -> c_int;
+        pub fn SDL_imageFilterShiftRightAndMultByByte(Src1: *u8,
+                                                      Dest: *u8,
+                                                      length: c_uint, N: u8,
+                                                      C: u8) -> c_int;
+        pub fn SDL_imageFilterShiftLeftByte(Src1: *u8,
+                                            Dest: *u8, length: c_uint,
+                                            N: u8) -> c_int;
+        pub fn SDL_imageFilterShiftLeftUint(Src1: *u8,
+                                            Dest: *u8, length: c_uint,
+                                            N: u8) -> c_int;
+        pub fn SDL_imageFilterShiftLeft(Src1: *u8, Dest: *u8,
+                                        length: c_uint, N: u8) -> c_int;
+        pub fn SDL_imageFilterBinarizeUsingThreshold(Src1: *u8,
+                                                     Dest: *u8,
+                                                     length: c_uint, T: u8)
          -> c_int;
-        pub fn SDL_imageFilterClipToRange(Src1: *mut c_uchar, Dest: *mut c_uchar,
-                                          length: c_uint, Tmin: c_uchar,
-                                          Tmax: c_uchar) -> c_int;
-        pub fn SDL_imageFilterNormalizeLinear(Src: *mut c_uchar,
-                                              Dest: *mut c_uchar, length: c_uint,
+        pub fn SDL_imageFilterClipToRange(Src1: *u8, Dest: *u8,
+                                          length: c_uint, Tmin: u8,
+                                          Tmax: u8) -> c_int;
+        pub fn SDL_imageFilterNormalizeLinear(Src: *u8,
+                                              Dest: *u8, length: c_uint,
                                               Cmin: c_int, Cmax: c_int,
                                               Nmin: c_int, Nmax: c_int) -> c_int;
     }
