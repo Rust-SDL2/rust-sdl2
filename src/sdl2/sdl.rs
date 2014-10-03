7b6e64986611452d89b64cdb8288e4968e8e4681
Joshua DeSeno <jdeseno@gmail.com>
2014-10-03 11:45:10 +0900

    Changes for latest rust
    Slice syntax
    Attribute resolution: `#[a] #[b] #[c] -> #[any(a,b,c)]`

--- src/sdl2/sdl.rs
+++ src/sdl2/sdl.rs
@@ -12,9 +12,7 @@ mod mac {
     extern {}
 }
 
-#[cfg(target_os="windows")]
-#[cfg(target_os="linux")]
-#[cfg(target_os="freebsd")]
+#[cfg(any(target_os="windows", target_os="linux", target_os="freebsd"))]
 mod others {
     #[link(name="SDL2")]
     extern {}
