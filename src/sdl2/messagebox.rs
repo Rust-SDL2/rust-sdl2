808082f684bf31aceff2436c98b9ce4463893d4c
Matthias Reitinger <reima@edenprime.de>
2014-11-18 14:55:18 +0100

    Replaced ll::SDL_MessageBoxFlags enum with constants.

--- src/sdl2/messagebox.rs
+++ src/sdl2/messagebox.rs
@@ -7,11 +7,10 @@ pub mod ll {
     use libc::{c_int, c_char, uint32_t};
     use video::ll::SDL_Window;
 
-    pub enum SDL_MessageBoxFlags {
-        SDL_MESSAGEBOX_ERROR        = 0x00000010,
-        SDL_MESSAGEBOX_WARNING      = 0x00000020,
-        SDL_MESSAGEBOX_INFORMATION  = 0x00000040
-    }
+    pub type SDL_MessageBoxFlags = u32;
+    pub const SDL_MESSAGEBOX_ERROR : SDL_MessageBoxFlags = 0x00000010;
+    pub const SDL_MESSAGEBOX_WARNING : SDL_MessageBoxFlags = 0x00000020;
+    pub const SDL_MESSAGEBOX_INFORMATION : SDL_MessageBoxFlags = 0x00000040;
 
     extern "C" {
         pub fn SDL_ShowSimpleMessageBox(flags: uint32_t, title: *const c_char, message: *const c_char, window: *const SDL_Window) -> c_int;
@@ -20,9 +19,9 @@ pub mod ll {
 
 bitflags! {
     flags MessageBoxFlag: u32 {
-        const MESSAGEBOX_ERROR = ll::SDL_MessageBoxFlags::SDL_MESSAGEBOX_ERROR as u32,
-        const MESSAGEBOX_WARNING = ll::SDL_MessageBoxFlags::SDL_MESSAGEBOX_WARNING as u32,
-        const MESSAGEBOX_INFORMATION = ll::SDL_MessageBoxFlags::SDL_MESSAGEBOX_INFORMATION as u32
+        const MESSAGEBOX_ERROR = ll::SDL_MESSAGEBOX_ERROR,
+        const MESSAGEBOX_WARNING = ll::SDL_MESSAGEBOX_WARNING,
+        const MESSAGEBOX_INFORMATION = ll::SDL_MESSAGEBOX_INFORMATION
     }
 }
 
