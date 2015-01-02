25abe03267ccafbab8f13c5240effc1db1aee431
Cary Haynie <cary.haynie@gmail.com>
2015-01-02 12:50:47 -0500

    move building via pkg-config to behind a feature.

--- sdl2-sys/build.rs
+++ sdl2-sys/build.rs
@@ -1,8 +1,12 @@
 extern crate "pkg-config" as pkg_config;
 
 fn main() {
-    if build_pkgconfig() { return; }
-    panic!("Could not find SDL2 via pkgconfig");
+    if std::os::getenv("CARGO_FEATURE_USE_PKGCONFIG").is_some() {
+      if build_pkgconfig() { return; }
+      panic!("Could not find SDL2 via pkgconfig");
+    } else {
+      println!("cargo:rustc-flags=-l SDL2");
+    }
 }
 
 fn build_pkgconfig() -> bool {
