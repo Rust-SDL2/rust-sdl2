2c22de475285fb8c07e37c163a0c665581140b2c
sbidin <sinisa@bidin.cc>
2015-03-23 01:57:26 +0100

    Make demo work with newest sdl2 and rust nightly.

--- src/demo/gfx_demo.rs
+++ src/demo/gfx_demo.rs
@@ -10,84 +10,86 @@ use sdl2::event::Event;
 use sdl2::pixels;
 use sdl2::keycode::KeyCode;
 
-
 use sdl2_gfx::primitives::DrawRenderer;
 use sdl2_gfx::framerate::FPSManager;
 
-static SCREEN_WIDTH : int = 800;
-static SCREEN_HEIGHT : int = 600;
+static SCREEN_WIDTH: i32 = 800;
+static SCREEN_HEIGHT: i32 = 600;
 
 // hadle the annoying Rect i32
 macro_rules! rect(
     ($x:expr, $y:expr, $w:expr, $h:expr) => (
         sdl2::rect::Rect::new($x as i32, $y as i32, $w as i32, $h as i32)
     )
-)
+);
 
 fn main() {
-    match mainloop() {
-        Ok(_) => (),
-        Err(e) => println!("error while running mainloop: {}", e),
-    }
-}
 
+    let context = sdl2::init(sdl2::INIT_VIDEO).unwrap();
 
-fn mainloop() -> Result<(), String> {
-    sdl2::init(sdl2::INIT_VIDEO);
+    let window = sdl2::video::Window::new(
+        "rust-sdl2_gfx: draw line & FPSManager",
+        sdl2::video::WindowPos::PosCentered,
+        sdl2::video::WindowPos::PosCentered,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        sdl2::video::OPENGL).unwrap();
 
-    let window = try!(sdl2::video::Window::new(
-        "rust-sdl2_gfx: draw line & FPSManager", sdl2::video::WindowPos::PosCentered,
-        sdl2::video::WindowPos::PosCentered, SCREEN_WIDTH, SCREEN_HEIGHT,
-        sdl2::video::OPENGL));
+    let renderer = sdl2::render::Renderer::from_window(
+        window,
+        sdl2::render::RenderDriverIndex::Auto,
+        sdl2::render::ACCELERATED).unwrap();
 
-    let renderer = try!(sdl2::render::Renderer::from_window(
-            window, sdl2::render::RenderDriverIndex::Auto, sdl2::render::ACCELERATED));
-
-    try!(renderer.set_draw_color(pixels::Color::RGB(0, 0, 0)));
-
-    try!(renderer.clear());
-
-    renderer.present();
+    let mut drawer = renderer.drawer();
+    drawer.set_draw_color(pixels::Color::RGB(0, 0, 0));
+    drawer.clear();
+    drawer.present();
 
     let mut rng = rand::XorShiftRng::new_unseeded();
     let (mut lastx, mut lasty) = (0, 0);
 
     let mut fpsm = FPSManager::new();
-    // default 30 is not good
-    try!(fpsm.set_framerate(100));
 
-    'main : loop {
-        'event : loop {
-            // this will avoid program to run 100% CPU
-            fpsm.delay();
+    fpsm.set_framerate(100); // Default 30 is not good.
 
-            match event::poll_event() {
-                Event::Quit(_) => break 'main,
-                Event::KeyDown(_, _, key, _, _, _) => {
+    let mut events = context.event_pump();
+
+    'main: loop {
+
+        fpsm.delay(); // This will avoid program to run 100% CPU.
+
+        for event in events.poll_iter() {
+
+            match event {
+
+                Event::Quit {..} => break 'main,
+
+                Event::KeyDown {keycode: key, ..} => {
                     if key == KeyCode::Escape {
                         break 'main
                     } else if key == KeyCode::Space {
-                        for i in range(0u, 400) {
-                            try!(renderer.pixel(i as i16, i as i16, 0xFF000FFu32));
+                        for i in 0..400 {
+                            renderer.pixel(i as i16, i as i16, 0xFF000FFu32);
                         }
-                        renderer.present();
+                        drawer.present();
 
                     }
                 }
-                Event::MouseButtonDown(_, _, _, _, x, y) => {
-                    let color : pixels::Color = Rand::rand(&mut rng);
-                    // println!("color => {:}", color);
-                    try!(renderer.line(lastx, lasty, x as i16, y as i16, color));
+
+                Event::MouseButtonDown {x: x, y: y, ..} => {
+                    let color = pixels::Color::RGB(
+                        Rand::rand(&mut rng),
+                        Rand::rand(&mut rng),
+                        Rand::rand(&mut rng));
+                    renderer.line(lastx, lasty, x as i16, y as i16, color);
                     lastx = x as i16;
                     lasty = y as i16;
                     println!("mouse btn down at ({},{})", x, y);
-                    renderer.present();
+                    drawer.present();
                 }
 
                 _ => {}
             }
         }
     }
-    sdl2::quit();
-    Ok(())
 }
