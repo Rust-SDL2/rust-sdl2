7109d800ee1047b4d45f7562ff8275ea1b9e0e93
Outfl3sh <stanislav.rosicky@gmail.com>
2015-03-27 21:26:49 +0100

    sdl2_sys import fix

--- src/sdl2_gfx/lib.rs
+++ src/sdl2_gfx/lib.rs
@@ -11,7 +11,7 @@ A binding for SDL2_gfx.
 
 extern crate libc;
 extern crate sdl2;
-extern crate "sdl2-sys" as sys;
+extern crate sdl2_sys as sys;
 
 // Setup linking for all targets.
 #[link(name="SDL2_gfx")]
