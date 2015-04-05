4ee9697f5ee92c03463ead72dc6ae402ded3db4c
Dan Spencer <dan@atomicpotato.net>
2015-04-04 23:04:24 -0600

    Add audio example in the documentation

---
+++ examples/audio-squarewave.rs
@@ -0,0 +1,54 @@
+extern crate sdl2;
+
+use sdl2::audio::{AudioCallback, AudioDevice, AudioSpecDesired};
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        // Generate a square wave
+        for x in out.iter_mut() {
+            *x = match self.phase {
+                0.0...0.5 => self.volume,
+                _ => -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
+}
+
+fn main() {
+    let _sdl_context = sdl2::init(sdl2::INIT_AUDIO).unwrap();
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1),  // mono
+        samples: None       // default sample size
+    };
+
+    let device = AudioDevice::open_playback(None, desired_spec, |spec| {
+        // Show obtained AudioSpec
+        println!("{:?}", spec);
+
+        // initialize the audio callback
+        SquareWave {
+            phase_inc: 440.0 / spec.freq as f32,
+            phase: 0.0,
+            volume: 0.25
+        }
+    }).unwrap();
+
+    // Start playback
+    device.resume();
+
+    // Play for 2 seconds
+    sdl2::timer::delay(2000);
+
+    // Device is automatically closed when dropped
+}
--- src/sdl2/audio.rs
+++ src/sdl2/audio.rs
@@ -1,4 +1,53 @@
 //! Audio Functions
+//!
+//! # Example
+//! ```no_run
+//! use sdl2::audio::{AudioCallback, AudioDevice, AudioSpecDesired};
+//!
+//! struct SquareWave {
+//!     phase_inc: f32,
+//!     phase: f32,
+//!     volume: f32
+//! }
+//!
+//! impl AudioCallback for SquareWave {
+//!     type Channel = f32;
+//!
+//!     fn callback(&mut self, out: &mut [f32]) {
+//!         // Generate a square wave
+//!         for x in out.iter_mut() {
+//!             *x = match self.phase {
+//!                 0.0...0.5 => self.volume,
+//!                 _ => -self.volume
+//!             };
+//!             self.phase = (self.phase + self.phase_inc) % 1.0;
+//!         }
+//!     }
+//! }
+//!
+//! let _sdl_context = sdl2::init(sdl2::INIT_AUDIO).unwrap();
+//!
+//! let desired_spec = AudioSpecDesired {
+//!     freq: Some(44100),
+//!     channels: Some(1),  // mono
+//!     samples: None       // default sample size
+//! };
+//!
+//! let device = AudioDevice::open_playback(None, desired_spec, |spec| {
+//!     // initialize the audio callback
+//!     SquareWave {
+//!         phase_inc: 440.0 / spec.freq as f32,
+//!         phase: 0.0,
+//!         volume: 0.25
+//!     }
+//! }).unwrap();
+//!
+//! // Start playback
+//! device.resume();
+//!
+//! // Play for 2 seconds
+//! sdl2::timer::delay(2000);
+//! ```
 use std::ffi::{CStr, CString};
 use std::num::FromPrimitive;
 use libc::{c_int, c_void, uint8_t};
