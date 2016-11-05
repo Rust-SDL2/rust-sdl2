017147b0da8d4d55acb0a1bec864ef1ac8f59630
Jake Kiesel <Jake Kiesel>
2016-11-05 11:18:42 -0600

    Well that didn't fix it so I'm undoing it.  I guess I'll have to install Linux to figure out why this is failing the build.

--- src/sdl2/event.rs
+++ src/sdl2/event.rs
@@ -23,8 +23,7 @@ use keyboard::Mod;
 use sys::keycode::SDL_Keymod;
 use keyboard::Keycode;
 use mouse;
-use mouse::{Mouse, MouseState};
-use mouse::MouseWheelDirection;
+use mouse::{Mouse, MouseState, MouseWheelDirection};
 use keyboard::Scancode;
 use get_error;
 
