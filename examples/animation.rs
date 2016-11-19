0e6dd1e6afcc8fca3133de78592f3672ed60beeb
Luke Jones <luke.nukem.jones@gmail.com>
2016-11-19 14:54:02 +1300

    Add animation example

---
+++ examples/animation.rs
@@ -0,0 +1,58 @@
+extern crate sdl2;
+use std::path::Path;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use std::time::Duration;
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("SDL2", 640, 480)
+        .position_centered().build().unwrap();
+
+    let mut renderer = window.renderer()
+        .software().build().unwrap();
+
+    renderer.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
+
+    let mut timer = sdl_context.timer().unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/animate.bmp")).unwrap();
+    let texture = renderer.create_texture_from_surface(temp_surface).unwrap();
+    let texture_query  = texture.query();
+
+    let mut source_rect = Rect::new(0, 0, 128, 82);
+    let dest_rect = Rect::new(0, 0, 128, 82);
+
+    let mut running = true;
+    while running {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    running = false;
+                },
+                _ => {}
+            }
+        }
+
+        let ticks = timer.ticks();
+
+        // Cycle through coords
+        // 0
+        // 128
+        // 384
+        // 512
+        // 640
+    //    source_rect.set_x((128 * ((ticks / 100) % 6) ) as i32);
+        renderer.clear();
+        renderer.copy_ex(&texture, Some(source_rect), Some(dest_rect), 0.0, None, true, false);
+        renderer.present();
+
+        std::thread::sleep(Duration::from_millis(100));
+    }
+}
