0e1bc28d1090d6db635e9e4abe70c5f123075d4c
Cobrand <cobrandw@gmail.com>
2016-12-02 20:54:09 +0100

    Fix mixer example

--- examples/mixer_demo.rs
+++ examples/mixer_demo.rs
@@ -1,9 +1,8 @@
 extern crate sdl2;
-extern crate sdl2_mixer;
 
 use std::env;
 use std::path::Path;
-use sdl2_mixer::{INIT_MP3, INIT_FLAC, INIT_MOD, INIT_FLUIDSYNTH, INIT_MODPLUG, INIT_OGG,
+use sdl2::mixer::{INIT_MP3, INIT_FLAC, INIT_MOD, INIT_FLUIDSYNTH, INIT_MODPLUG, INIT_OGG,
                  AUDIO_S16LSB};
 
 fn main() {
@@ -19,12 +18,12 @@ fn main() {
 
 fn demo(filename: &Path) {
 
-    println!("linked version: {}", sdl2_mixer::get_linked_version());
+    println!("linked version: {}", sdl2::mixer::get_linked_version());
 
     let sdl = sdl2::init().unwrap();
     let _audio = sdl.audio().unwrap();
     let mut timer = sdl.timer().unwrap();
-    let _mixer_context = sdl2_mixer::init(INIT_MP3 | INIT_FLAC | INIT_MOD | INIT_FLUIDSYNTH |
+    let _mixer_context = sdl2::mixer::init(INIT_MP3 | INIT_FLAC | INIT_MOD | INIT_FLUIDSYNTH |
                                           INIT_MODPLUG |
                                           INIT_OGG)
                             .unwrap();
@@ -33,44 +32,44 @@ fn demo(filename: &Path) {
     let format = AUDIO_S16LSB; // signed 16 bit samples, in little-endian byte order
     let channels = 2; // Stereo
     let chunk_size = 1024;
-    let _ = sdl2_mixer::open_audio(frequency, format, channels, chunk_size).unwrap();
-    sdl2_mixer::allocate_channels(0);
+    let _ = sdl2::mixer::open_audio(frequency, format, channels, chunk_size).unwrap();
+    sdl2::mixer::allocate_channels(0);
 
     {
-        let n = sdl2_mixer::get_chunk_decoders_number();
+        let n = sdl2::mixer::get_chunk_decoders_number();
         println!("available chunk(sample) decoders: {}", n);
         for i in 0..n {
-            println!("  decoder {} => {}", i, sdl2_mixer::get_chunk_decoder(i));
+            println!("  decoder {} => {}", i, sdl2::mixer::get_chunk_decoder(i));
         }
     }
 
     {
-        let n = sdl2_mixer::get_music_decoders_number();
+        let n = sdl2::mixer::get_music_decoders_number();
         println!("available music decoders: {}", n);
         for i in 0..n {
-            println!("  decoder {} => {}", i, sdl2_mixer::get_music_decoder(i));
+            println!("  decoder {} => {}", i, sdl2::mixer::get_music_decoder(i));
         }
     }
 
-    println!("query spec => {:?}", sdl2_mixer::query_spec());
+    println!("query spec => {:?}", sdl2::mixer::query_spec());
 
 
-    let music = sdl2_mixer::Music::from_file(filename).unwrap();
+    let music = sdl2::mixer::Music::from_file(filename).unwrap();
 
     fn hook_finished() {
         println!("play ends! from rust cb");
     }
 
-    sdl2_mixer::Music::hook_finished(hook_finished);
+    sdl2::mixer::Music::hook_finished(hook_finished);
 
     println!("music => {:?}", music);
     println!("music type => {:?}", music.get_type());
-    println!("music volume => {:?}", sdl2_mixer::Music::get_volume());
+    println!("music volume => {:?}", sdl2::mixer::Music::get_volume());
     println!("play => {:?}", music.play(1));
 
     timer.delay(10000);
 
-    println!("fading out ... {:?}", sdl2_mixer::Music::fade_out(4000));
+    println!("fading out ... {:?}", sdl2::mixer::Music::fade_out(4000));
 
     timer.delay(5000);
 
@@ -78,7 +77,7 @@ fn demo(filename: &Path) {
              music.fade_in_from_pos(1, 10000, 100.0));
 
     timer.delay(5000);
-    sdl2_mixer::Music::halt();
+    sdl2::mixer::Music::halt();
     timer.delay(1000);
 
 
