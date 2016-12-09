67c99f18012371bbe0cc7e46bb9eb7f7fe9dd7ca
Kyle Strohbeck <kyles@callbox.com>
2016-12-09 13:11:42 -0600

    Implement Display for PrefPathError.

--- src/sdl2/filesystem.rs
+++ src/sdl2/filesystem.rs
@@ -1,4 +1,5 @@
 use std::ffi::{CStr, CString, NulError};
+use std::fmt;
 use get_error;
 use libc::c_char;
 
@@ -22,6 +23,18 @@ pub enum PrefPathError {
     InvalidOrganizationName(NulError),
     InvalidApplicationName(NulError),
     SdlError(String),
+}
+
+impl fmt::Display for PrefPathError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::PrefPathError::*;
+
+        match *self {
+            InvalidOrganizationName(ref e) => write!(f, "Invalid organization name: {}", e),
+            InvalidApplicationName(ref e) => write!(f, "Invalid application name: {}", e),
+            SdlError(ref e) => write!(f, "SDL error: {}", e)
+        }
+    }
 }
 
 // TODO: Change to OsStr or something?
