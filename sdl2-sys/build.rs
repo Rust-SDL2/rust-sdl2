761a260e5940591df2388733e8c5d90a46faba13
Michael Fairley <michaelfairley@gmail.com>
2017-07-22 22:55:22 -0500

    Add link directives for iOS frameworks

--- sdl2-sys/build.rs
+++ sdl2-sys/build.rs
@@ -2,15 +2,28 @@
 extern crate pkg_config;
 
 fn main() {
-    if !build_pkgconfig() {
-        let target = ::std::env::var("TARGET").expect("Cargo build scripts always have TARGET");
-        let target_os = target.splitn(3, "-").nth(2).unwrap();
+    let target = ::std::env::var("TARGET").expect("Cargo build scripts always have TARGET");
+    let target_os = target.splitn(3, "-").nth(2).unwrap();
 
+    if !build_pkgconfig() {
         if cfg!(feature="use_mac_framework") && target_os == "darwin" {
             println!("cargo:rustc-flags=-l framework=SDL2");
         } else {
             println!("cargo:rustc-flags=-l SDL2");
         }
+    }
+
+    if target_os == "ios" {
+        println!("cargo:rustc-flags=-l framework=AVFoundation");
+        println!("cargo:rustc-flags=-l framework=AudioToolbox");
+        println!("cargo:rustc-flags=-l framework=CoreAudio");
+        println!("cargo:rustc-flags=-l framework=CoreGraphics");
+        println!("cargo:rustc-flags=-l framework=CoreMotion");
+        println!("cargo:rustc-flags=-l framework=Foundation");
+        println!("cargo:rustc-flags=-l framework=GameController");
+        println!("cargo:rustc-flags=-l framework=OpenGLES");
+        println!("cargo:rustc-flags=-l framework=QuartzCore");
+        println!("cargo:rustc-flags=-l framework=UIKit");
     }
 }
 
