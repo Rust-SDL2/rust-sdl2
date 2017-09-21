0f44d4359586e30e2d1f01de65cd2340120874f1
Joshua Erney <joshua.erney@gmail.com>
2017-09-21 11:33:06 -0500

    Fix floating-point literal used in pattern in example (#705)

--- examples/audio-squarewave.rs
+++ examples/audio-squarewave.rs
@@ -15,10 +15,7 @@ impl AudioCallback for SquareWave {
     fn callback(&mut self, out: &mut [f32]) {
         // Generate a square wave
         for x in out.iter_mut() {
-            *x = match self.phase {
-                0.0...0.5 => self.volume,
-                _ => -self.volume
-            };
+            *x = if self.phase < 0.5 { self.volume } else { -self.volume };
             self.phase = (self.phase + self.phase_inc) % 1.0;
         }
     }
