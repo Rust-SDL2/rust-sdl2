86fc6fbddcc363dc09283f897abf684858feb21c
Cobrand <cobrandw@gmail.com>
2017-11-12 23:25:49 +0000

    Add text in main doc page

--- src/sdl2/lib.rs
+++ src/sdl2/lib.rs
@@ -1,3 +1,48 @@
+//! # Getting started
+//!
+//! ```rust
+//! use sdl2::pixels::Color;
+//! use sdl2::event::Event;
+//! use sdl2::keyboard::Keycode;
+//! use std::time::Duration;
+//! 
+//! pub fn main() {
+//!     let sdl_context = sdl2::init().unwrap();
+//!     let video_subsystem = sdl_context.video().unwrap();
+//! 
+//!     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+//!         .position_centered()
+//!         .build()
+//!         .unwrap();
+//! 
+//!     let mut canvas = window.into_canvas().build().unwrap();
+//! 
+//!     canvas.set_draw_color(Color::RGB(0, 255, 255));
+//!     canvas.clear();
+//!     canvas.present();
+//!     let mut event_pump = sdl_context.event_pump().unwrap();
+//!     let mut i = 0;
+//!     'running: loop {
+//!         i = (i + 1) % 255;
+//!         canvas.set_draw_color(Color::RGB(i, 64, 255 - i))
+//!         canvas.clear();
+//!         for event in event_pump.poll_iter() {
+//!             match event {
+//!                 Event::Quit {..} |
+//!                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+//!                     break 'running
+//!                 },
+//!                 _ => {}
+//!             }
+//!         }
+//!         // The rest of the game loop goes here...
+//!
+//!         canvas.present();
+//!         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+//!     }
+//! }
+//! ```
+
 #![crate_name = "sdl2"]
 #![crate_type = "lib"]
 
