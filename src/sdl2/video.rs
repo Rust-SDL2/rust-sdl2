c555f55f58164b0acda6479e059ab6b33a1d4f4d
Tim Diekmann <t.diekmann.3dv@gmail.com>
2017-11-30 11:13:35 +0100

    Update border_size documentation

--- src/sdl2/video.rs
+++ src/sdl2/video.rs
@@ -1161,6 +1161,13 @@ impl Window {
     }
     
     /// Use this function to get the size of a window's borders (decorations) around the client area.
+    ///
+    /// # Remarks
+    /// This function is only supported on X11.
+    ///
+    /// #Notes
+    ///
+    /// If this function fails, (0, 0, 0, 0) will be returned
     pub fn border_size(&self) -> (u16, u16, u16, u16) {
         let mut top: c_int = 0;
         let mut left: c_int = 0;
