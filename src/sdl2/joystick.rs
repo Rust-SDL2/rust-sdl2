64a936c9459fdb5e53ff3a38471254060d234f08
Niels Wießner <n.h.wiessner@gmail.com>
2018-12-12 02:20:36 +0100

    Add some notes to `set_rumble`'s docs

--- src/sdl2/joystick.rs
+++ src/sdl2/joystick.rs
@@ -347,6 +347,15 @@ impl Joystick {
 
     /// Set the rumble motors to their specified intensities, if supported.
     /// Automatically resets back to zero after `duration_ms` milliseconds have passed.
+    ///
+    /// # Notes
+    ///
+    /// The value range for the intensities is 0 to 0xFFFF.
+    ///
+    /// Do *not* use `std::u32::MAX` or similar for `duration_ms` if you want
+    /// the rumble effect to keep playing for a long time, as this results in
+    /// the effect ending immediately after starting due to an overflow.
+    /// Use some smaller, "huge enough" number instead.
     pub fn set_rumble(&mut self,
                       low_frequency_rumble: u16,
                       high_frequency_rumble: u16,
