f7da4652625025e8b647d08d9619107431c45ffc
Giovanni Bajo <rasky@develer.com>
2018-12-29 19:21:03 +0100

    Add missing shell32 to static link libraries on Windows

--- sdl2-sys/build.rs
+++ sdl2-sys/build.rs
@@ -315,6 +315,7 @@ fn link_sdl2(target_os: &str) {
 
         // Also linked to any required libraries for each supported platform
         if target_os.contains("windows") {
+            println!("cargo:rustc-link-lib=shell32");
             println!("cargo:rustc-link-lib=user32");
             println!("cargo:rustc-link-lib=gdi32");
             println!("cargo:rustc-link-lib=winmm");
