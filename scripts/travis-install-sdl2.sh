2f9f7469125aa06692568fc86dca20f7c457f00c
Melody Horn <melody@boringcactus.com>
2019-01-24 15:11:44 -0700

    restructure build logic

--- scripts/travis-install-sdl2.sh
+++ scripts/travis-install-sdl2.sh
@@ -2,11 +2,21 @@
 
 set -xueo pipefail
 
+function build() {
+    if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+        ./configure --build=x86_64-mingw32
+        mingw32-make
+        sudo mingw32-make install
+    else
+        ./configure
+        make
+        sudo make install
+    fi
+}
+
 wget https://www.libsdl.org/release/SDL2-2.0.9.tar.gz -O sdl2.tar.gz
 tar xzf sdl2.tar.gz
-if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then CONFIG_BUILD="--build=x86_64-mingw32"; else CONFIG_BUILD=; fi
-pushd SDL2-* && ./configure ${CONFIG_BUILD} && make && sudo make install && popd
-exit
+(pushd SDL2-* && build && popd) || exit
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
     EXT=.zip
     EXTRACT=unzip
@@ -22,7 +32,7 @@ ${EXTRACT} SDL2_ttf-*${EXT} && rm SDL2_ttf-*${EXT}
 ${EXTRACT} SDL2_image-*${EXT} && rm SDL2_image-*${EXT}
 ${EXTRACT} SDL2_mixer-*${EXT} && rm SDL2_mixer-*${EXT}
 ${EXTRACT} SDL2_gfx-*${EXT} && rm SDL2_gfx-*${EXT}
-pushd SDL2_ttf-* && ./configure && make && sudo make install && popd
-pushd SDL2_image-* && ./configure && make && sudo make install && popd
-pushd SDL2_mixer-* && ./configure && make && sudo make install && popd
-pushd SDL2_gfx-* && ./autogen.sh && ./configure && make && sudo make install && popd
+pushd SDL2_ttf-* && build && popd
+pushd SDL2_image-* && build && popd
+pushd SDL2_mixer-* && build && popd
+pushd SDL2_gfx-* && ./autogen.sh && build && popd
