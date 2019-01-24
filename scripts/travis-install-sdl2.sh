ec59b506e7038efa013d7fa0e75c1f57c9f33d9a
Melody Horn <melody@boringcactus.com>
2019-01-24 14:07:42 -0700

    delete archives after extracting to not confuse pushd

--- scripts/travis-install-sdl2.sh
+++ scripts/travis-install-sdl2.sh
@@ -16,10 +16,10 @@ wget -q https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14${EXT}
 wget -q https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1${EXT}
 wget -q https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.2${EXT}
 wget -q -O SDL2_gfx-1.0.1${EXT} https://sourceforge.net/projects/sdl2gfx/files/SDL2_gfx-1.0.1${EXT}/download
-${EXTRACT} SDL2_ttf-*${EXT}
-${EXTRACT} SDL2_image-*${EXT}
-${EXTRACT} SDL2_mixer-*${EXT}
-${EXTRACT} SDL2_gfx-*${EXT}
+${EXTRACT} SDL2_ttf-*${EXT} && rm SDL2_ttf-*${EXT}
+${EXTRACT} SDL2_image-*${EXT} && rm SDL2_image-*${EXT}
+${EXTRACT} SDL2_mixer-*${EXT} && rm SDL2_mixer-*${EXT}
+${EXTRACT} SDL2_gfx-*${EXT} && rm SDL2_gfx-*${EXT}
 pushd SDL2_ttf-* && ./configure && make && sudo make install && popd
 pushd SDL2_image-* && ./configure && make && sudo make install && popd
 pushd SDL2_mixer-* && ./configure && make && sudo make install && popd
