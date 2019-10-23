4e37c1b789385583684c44e423f3938913e37f13
Jakub Stasiak <jakub@stasiak.at>
2019-10-23 02:49:50 +0200

    Fix clicking and volume control in the audio-wav example

--- examples/audio-wav.rs
+++ examples/audio-wav.rs
@@ -19,7 +19,20 @@ impl AudioCallback for Sound {
 
     fn callback(&mut self, out: &mut [u8]) {
         for dst in out.iter_mut() {
-            *dst = (*self.data.get(self.pos).unwrap_or(&0) as f32 * self.volume) as u8;
+            // With channel type u8 the "silence" value is 128 (middle of the 0-2^8 range) so we need
+            // to both fill in the silence and scale the wav data accordingly. Filling the silence
+            // once the wav is finished is trivial, applying the volume is more tricky. We need to:
+            // * Change the range of the values from [0, 255] to [-128, 127] so we can multiply
+            // * Apply the volume by multiplying, this gives us range [-128*volume, 127*volume]
+            // * Move the resulting range to a range centered around the value 128, the final range
+            //   is [128 - 128*volume, 128 + 127*volume] – scaled and correctly positioned
+            //
+            // Using value 0 instead of 128 would result in clicking. Scaling by simply multiplying
+            // would not give correct results.
+            let pre_scale = *self.data.get(self.pos).unwrap_or(&128);
+            let scaled_signed_float = (pre_scale as f32 - 128.0) * self.volume;
+            let scaled = (scaled_signed_float + 128.0) as u8;
+            *dst = scaled;
             self.pos += 1;
         }
     }
