57775c4e5d079d8b4d6c8cb401b6e5d42ee9cee4
Alex Helfet <alex.helfet@gmail.com>
2019-11-18 23:29:07 +0000

    Error handling in examples/animation without failure.

--- examples/animation.rs
+++ examples/animation.rs
@@ -1,16 +1,14 @@
-extern crate failure;
 extern crate sdl2;
 
 use std::path::Path;
 
-use failure::Error;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
 use sdl2::rect::Point;
 use std::time::Duration;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
